653c9db95037bbb6a880e1af4b2738d9f6cabc5a
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-11-25 23:55:07 +0530

    Added home and about pages as React components

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,85 @@
 "use strict";
 
-var gulp = require('gulp');
-var connect = require('gulp-connect'); // Runs a local dev server
-var open = require('gulp-open'); // Open a URL in a web browser
-var browserify = require('browserify'); // Bundles JS
-var reactify = require('reactify'); // Transforms React JSX to JS
-var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
-var concat = require('gulp-concat'); // Concatenates files
-var lint = require('gulp-eslint'); // Lint JS files, including JSX
+var gulp = require("gulp");
+var connect = require("gulp-connect"); // Runs a local dev server
+var open = require("gulp-open"); // Open a URL in a web browser
+var browserify = require("browserify"); // Bundles JS
+var reactify = require("reactify"); // Transforms React JSX to JS
+var source = require("vinyl-source-stream"); // Use conventional text streams with Gulp
+var concat = require("gulp-concat"); // Concatenates files
+var lint = require("gulp-eslint"); // Lint JS files, including JSX
 
 var config = {
     port: 9005,
-    devBaseUrl: 'http://localhost',
+    devBaseUrl: "http://localhost",
     paths: {
-        html: './src/*.html',
-        js: './src/**/*.js',
+        html: "./src/*.html",
+        js: "./src/**/*.js*",
         css: [
-            'node_modules/bootstrap/dist/css/bootstrap.min.css',
-            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+            "node_modules/bootstrap/dist/css/bootstrap.min.css",
+            "node_modules/bootstrap/dist/css/bootstrap-theme.min.css"
         ],
-        dist: './dist',
-        mainJs: './src/main.js'
+        dist: "./dist",
+        mainJs: "./src/main.js"
     }
 }
 // Start a local development server
-gulp.task('connect', function () {
+gulp.task("connect", function () {
     connect.server({
-        root: ['dist'],
+        root: ["dist"],
         port: config.port,
         base: config.devBaseUrl,
         livereload: true
-    })
+    });
 });
 
 // Open the app in the web browser
-gulp.task('open', ['connect'], function () {
-    gulp.src('dist/index.html')
+gulp.task("open", ["connect"], function () {
+    gulp.src("dist/index.html")
         .pipe(open({
-            uri: config.devBaseUrl + ':' + config.port + '/'
+            uri: config.devBaseUrl + ":" + config.port + "/"
         }));
 });
 
 // Copy the source code to the distribution folder
-gulp.task('html', function () {
+gulp.task("html", function () {
     gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.dist))
         .pipe(connect.reload());
 });
 
 // Transform JSX, bundle the Javascript and copy to distribution folder
-gulp.task('js', function () {
+gulp.task("js", function () {
     browserify(config.paths.mainJs)
         .transform(reactify)
         .bundle()
-        .on('error', console.error.bind(console))
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .on("error", console.error.bind(console))
+        .pipe(source("bundle.js"))
+        .pipe(gulp.dest(config.paths.dist + "/scripts"))
         .pipe(connect.reload());
 });
 
 // Bundle the Bootstrap CSS files
-gulp.task('css', function () {
+gulp.task("css", function () {
     gulp.src(config.paths.css)
-        .pipe(concat('bundle.css'))
-        .pipe(gulp.dest(config.paths.dist + '/css'));
+        .pipe(concat("bundle.css"))
+        .pipe(gulp.dest(config.paths.dist + "/css"));
 });
 
 // Lint (validate) Javascript and JSX files
-gulp.task('lint', function () {
+gulp.task("lint", function () {
     return gulp.src(config.paths.js)
         .pipe(lint({
-            config: 'eslint.config.json'
+            config: "eslint.config.json"
         }))
         .pipe(lint.format());
 });
 
 // Monitor changes to the HTML and Javascript files
-gulp.task('watch', function () {
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js', 'lint']);
+gulp.task("watch", function () {
+    gulp.watch(config.paths.html, ["html"]);
+    gulp.watch(config.paths.js, ["js", "lint"]);
 });
 
 // Default task
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
+gulp.task("default", ["html", "js", "css", "lint", "open", "watch"]);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
-$ = jQuery = require('jquery');
+$ = jQuery = require("jquery");
 
-var App = console.log('Hello from browersify');
+var React = require("react");
+var Home = require("./components/homePage.jsx");
+var About = require("./components/about/aboutPage.jsx");
 
-module.exports = App;
+var App = React.createClass({
+    render: function() {
+        var Child;
+
+        switch(this.props.route) {
+            case "about":
+                Child = About;
+                break;
+            default:
+                Child = Home;
+        }
+
+        return (
+            <div>
+                <Child />
+            </div>
+        );
+    }
+});
+
+function render() {
+    var route = window.location.hash.substr(1);
+    React.render(<App route={route} />, 
+            document.getElementById("app"));
+}
+
+window.addEventListener("hashchange", render);
+
+render();
