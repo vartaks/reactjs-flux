aac3b6c37a168b9962dffd363eac163c56d11036
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-11-25 22:09:45 +0530

    Added ESLint to validate Javascript and JSX files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify'); // Bundles JS
 var reactify = require('reactify'); // Transforms React JSX to JS
 var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
 var concat = require('gulp-concat'); // Concatenates files
+var lint = require('gulp-eslint'); // Lint JS files, including JSX
 
 var config = {
     port: 9005,
@@ -59,17 +60,26 @@ gulp.task('js', function () {
 });
 
 // Bundle the Bootstrap CSS files
-gulp.task('css', function() {
+gulp.task('css', function () {
     gulp.src(config.paths.css)
         .pipe(concat('bundle.css'))
         .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
+// Lint (validate) Javascript and JSX files
+gulp.task('lint', function () {
+    return gulp.src(config.paths.js)
+        .pipe(lint({
+            config: 'eslint.config.json'
+        }))
+        .pipe(lint.format());
+});
+
 // Monitor changes to the HTML and Javascript files
 gulp.task('watch', function () {
     gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
 // Default task
-gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
