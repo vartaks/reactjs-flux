672d80186a48c0be866079684f40ed3aca8e3394
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-11-30 23:13:49 +0530

    Added examples for willTransitionTo and willTransitionFrom hooks

--- src/components/about/aboutPage.jsx
+++ src/components/about/aboutPage.jsx
@@ -3,6 +3,21 @@
 var React = require("react");
 
 var About = React.createClass({
+	statics: {
+		willTransitionTo: function(transition, params, query, callback) {
+			if (!confirm('Are you sure to read about us?')) {
+				transition.abort();
+			} else {
+				callback();
+			}
+		},
+
+		willTransitionFrom: function(transition, component) {
+			if (!confirm('Are you sure you finished reading about us?')) {
+				transition.abort();
+			}
+		}
+	},
     render: function() {
         return (
             <div>
