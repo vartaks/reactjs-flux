16fa3d277ed4ba50c6f3b4585f6955b3af0522be
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-01 14:06:06 +0530

    Added confirmation for unsaved form data

--- src/components/authors/authorForm.jsx
+++ src/components/authors/authorForm.jsx
@@ -4,6 +4,13 @@ var React = require("react");
 var Input = require("../common/textInput.jsx");
 
 var AuthorForm = React.createClass({
+    propTypes: {
+		author:	React.PropTypes.object.isRequired,
+		onSave:	React.PropTypes.func.isRequired,
+		onChange: React.PropTypes.func.isRequired,
+		errors: React.PropTypes.object
+    },
+    
     render: function () {
         return (
             <form>
--- src/components/authors/manageAuthorPage.jsx
+++ src/components/authors/manageAuthorPage.jsx
@@ -11,14 +11,25 @@ var ManageAuthorPage = React.createClass({
         Router.Navigation
     ],
 
+    statics: {
+        willTransitionFrom: function(transition, component) {
+            if (component.state.dirty && !confirm("Leave without saving?")) {
+                transition.abort();
+            }
+        }
+    },
+
     getInitialState: function() {
         return {
             author: { id: "", firstName: "", lastName: ""},
-            errors: {}
+            errors: {},
+            dirty: false
         };
     },
 
     setAuthorState: function(event) {
+        this.setState({dirty: true});
+
         var field = event.target.name;
         var value = event.target.value;
 
@@ -55,6 +66,7 @@ var ManageAuthorPage = React.createClass({
         }
 
         AuthorApi.saveAuthor(this.state.author);
+        this.setState({dirty: false});
         toastr.success("Author saved.");
         this.transitionTo("authors");
     },
